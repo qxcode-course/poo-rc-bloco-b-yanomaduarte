939865b30160c70c407c36cda429ea090eeb46e1
yanduarteNOTE <yanduarte13@gmail.com>
2025-10-22 08:26:32 -0300

    questChinela solved

--- myrep/poo/chinela/py/draft.py
+++ myrep/poo/chinela/py/draft.py
@@ -6,29 +6,22 @@ class Chinela:
         return self.__tamanho
 
     def setTamanho(self, valor: int):
-        tamanho = valor
-        valor = int(input("Digite seu tamanho de chinela: "))
         if valor < 20 or valor > 50:
             print("Tamanho inválido. Digite um tamanho entre 20 e 50.")
-            self.__valor = 0
-            return False
+            return
 
         if valor % 2 != 0:
             print("Tamanho inválido. Digite um tamanho par entre 20 e 50.")
-            self.__valor = 0
-            return False
+            return
 
-        self.__valor = valor
-        
-        return True
+        self.__tamanho = valor
+
 
 chinela = Chinela()
 
+while chinela.getTamanho() == 0:
+    print("Digite se tamanho de chinela")
+    tamanho = int(input())
+    chinela.setTamanho(tamanho)
 
-def main():
-    chinela = Chinela()
-    chinela.setTamanho(valor)
-    print("Parabéns, você comprou uma chinela tamanho", chinela.getTamanho())
-
-if __name__ == '__main__':
-    main()
+print("Parabéns, você comprou uma chinela tamanho", chinela.getTamanho())
