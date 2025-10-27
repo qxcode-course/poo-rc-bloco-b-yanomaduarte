73d3949d363d06bf40a2d06e25303fb282b2db13
yanomaduarte <yanduarte13@gmail.com>
2025-10-27 10:13:04 -0300

    questMotouber solved

--- myrep/poo/motouber/py/draft.py
+++ myrep/poo/motouber/py/draft.py
@@ -9,11 +9,12 @@ class Person:
     def getMoney(self):
         return self.__money
 
-    def setMoney(self):
+    def setMoney(self, value):
         self.__money = value
 
     def __str__(self):
         return f"{self.__name}:{int(self.__money)}"
+
 
 class Motorcycle:
     def __init__(self):
@@ -24,18 +25,17 @@ class Motorcycle:
     def setDriver(self, person: Person):
         self.__driver = person
 
-    def setPassenger(self, person: Person):
+    def setPass(self, person: Person):
         self.__passenger = person
 
     def drive(self, km: int):
         if not self.__driver:
             print("fail: não há motorista")
             return
-
         if not self.__passenger:
             print("fail: não há passageiro")
-
-        self.__cost = km
+            return
+        self.__cost += km
 
     def leavePass(self):
         if not self.__passenger:
@@ -49,18 +49,22 @@ class Motorcycle:
         if passenger.getMoney() >= run_price:
             passenger.setMoney(passenger.getMoney() - run_price)
             driver.setMoney(driver.getMoney() + run_price)
-        
-        else:driver.setMoney(driver.getMoney() + run_price)
-        passenger.setMoney(0)
+
+        else:
+            print("fail: Passenger does not have enough money")
+            driver.setMoney(driver.getMoney() + run_price)
+            passenger.setMoney(0)
 
         print(f"{passenger.getName()}:{int(passenger.getMoney())} left")
 
         self.__passenger = None
         self.__cost = 0
 
-        def show(self):
-            driver = str(self.__driver) if self.__driver else "None"
-            passenger = str(self.__passenger) if self.__passenger else "None"
+    def show(self):
+        driver_str = str(self.__driver) if self.__driver else "None"
+        passenger_str = str(self.__passenger) if self.__passenger else "None"
+        print(
+            f"Cost: {self.__cost}, Driver: {driver_str}, Passenger: {passenger_str}")
 
 
 def main():
@@ -84,9 +88,9 @@ def main():
             motorcycle.setDriver(Person(name, money))
 
         elif command == "setPass":
-            name = args[2]
+            name = args[1]
             money = float(args[2])
-            motorcycle.setPassenger(Person(name, money))
+            motorcycle.setPass(Person(name, money))
 
         elif command == "drive":
             km = int(args[1])
@@ -97,6 +101,7 @@ def main():
 
         else:
             print("fail: comando inválido")
+
 
 if __name__ == "__main__":
     main()
